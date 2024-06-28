c65fff272cc069a479b3c7a88004c2545bc12288
Harrison Cook <HonsonCooky@gmail.com>
2024-06-28 15:07:23 +1200

    start language select

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,23 +1,79 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const langDropDownActive = ref(false);
+
+const languageSelectToggle = () =>
+  (langDropDownActive.value = !langDropDownActive.value);
+
+window.addEventListener("keydown", (event) => {
+  if (event.key === "l") languageSelectToggle();
+});
+</script>
+
 <template>
-  <header @keyup.enter="toggleLanguageDropDown">
+  <header>
     <hgroup>
-      <h2>Programmer See</h2>
-      <h1>Programmer Type</h1>
+      <h2>&lt; Programmer See /&gt;</h2>
+      <h1>&lt; Programmer Type /&gt;</h1>
     </hgroup>
+    <div id="banner">
+      <div id="language-select">
+        <label>[L]anguage</label>
+        <button @click="languageSelectToggle">
+          {}
+          <v-icon name="bi-chevron-down" />
+        </button>
+        <div class="dropdown" :class="{ active: langDropDownActive }">
+          <button>
+            <v-icon name="si-typescript" />
+            TypeScript
+          </button>
+          <button>C#</button>
+          <button>PowerShell</button>
+          <button>Windows OS Naviagtion</button>
+        </div>
+      </div>
+    </div>
   </header>
 </template>
 
-<script lang="ts">
-export default {
-  name: "HeaderBar",
-  methods: {
-    toggleLanguageDropDown(event: Event) {
-      console.log("lol", event);
-    },
-  },
-};
-</script>
-
 <style scoped>
-@import url("../App.css");
+header {
+  display: flex;
+  flex-direction: column;
+  padding: var(--s-m);
+
+  hgroup {
+    flex: 4;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+
+    h1 {
+      font-size: var(--fs-5);
+      font-weight: bold;
+      margin: 0;
+    }
+
+    h2 {
+      font-size: var(--fs-2);
+      font-weight: bold;
+      margin: 0;
+    }
+  }
+
+  #banner {
+    flex: 1;
+    display: flex;
+    flex-direction: row;
+    text-align: center;
+
+    #language-select {
+      display: flex;
+      flex-direction: row;
+      row-gap: var(--s-3xs);
+    }
+  }
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 
-createApp(App).mount("#app");
+import { addIcons, OhVueIcon } from "oh-vue-icons";
+import { BiChevronDown, SiTypescript } from "oh-vue-icons/icons";
+
+addIcons(BiChevronDown, SiTypescript);
+
+const app = createApp(App);
+app.component("v-icon", OhVueIcon);
+app.mount("#app");
