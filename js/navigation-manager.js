c7c49fde369d6ecdf8f5ac3d1eda8d6e61eecf43
Harrison Cook <HonsonCooky@gmail.com>
2024-07-11 23:09:11 +1200

    start keyboard manager for navigation

--- js/navigation-manager.js
+++ js/navigation-manager.js
@@ -1,60 +1,100 @@
 import { ProblemSetManager } from "./problem-set-manager.js";
 
+class NavigationKeyboardManager {
+	keyMappings = {
+		d: {
+			element: document.getElementById("duration-select"),
+			0: {
+				element: document.getElementById("duration-15"),
+				action: "click",
+			},
+			1: {
+				element: document.getElementById("duration-30"),
+				action: "click",
+			},
+			2: {
+				element: document.getElementById("duration-60"),
+				action: "click",
+			},
+			3: {
+				element: document.getElementById("duration-90"),
+				action: "click",
+			},
+			4: {
+				element: document.getElementById("duration-120"),
+				action: "click",
+			},
+			5: {
+				element: document.getElementById("duration-infinite"),
+				action: "click",
+			},
+		},
+		h: {
+			element: document.getElementById("help"),
+		},
+		p: {
+			element: document.getElementById("duration-select"),
+		},
+	};
+}
+
 export class NavigationMangager {
-  /**
-   * @param {Object} param0
-   * @param {ProblemSetManager} param0.problemSetManager
-   */
-  constructor({ problemSetManager }) {
-    this.problemSetManager = problemSetManager;
-  }
 
-  keymappings = {
-    d: {
-      element: document.getElementById("duration-select"),
-    },
-    p: {
-      element: document.getElementById("duration-select"),
-    },
-  };
+	/**
+	 * @param {Object} param0
+	 * @param {ProblemSetManager} param0.problemSetManager
+	 */
+	constructor({ problemSetManager }) {
+		this.problemSetManager = problemSetManager;
+		this.currentKeyMap = this.keyMappings;
+	}
 
-  _onSetsLoaded() {
-    const problemSetDropdownContent = document.getElementById("problem-set-select").querySelector(".dropdown-content");
-    for (const set of this.problemSetManager.sets) {
-      const btn = document.createElement("button");
-      btn.innerText = set.title;
-      btn.addEventListener("click", () => {
-        this.problemSetManager.selectSet(set);
-      });
-      problemSetDropdownContent.appendChild(btn);
-    }
-  }
 
-  _onSetSelected() {
-    // Update Label
-    const problemSetLabel = document.getElementById("problem-set-selected");
-    problemSetLabel.innerText = this.problemSetManager.selectedSet.title;
+	_onSetsLoaded() {
+		const problemSetDropdownContent = document.getElementById("problem-set-select").querySelector(".dropdown-content");
+		for (const set of this.problemSetManager.sets) {
+			const btn = document.createElement("button");
+			btn.innerText = set.title;
+			btn.addEventListener("click", () => {
+				this.problemSetManager.selectSet(set);
+			});
+			problemSetDropdownContent.appendChild(btn);
+		}
+	}
 
-    // Highlight Selected
-    Array.from(document.getElementById("problem-set-select").querySelector(".dropdown-content").children).forEach(
-      (child) => {
-        if (child.innerText === this.problemSetManager.selectedSet.title) child.style.color = "var(--rose)";
-        else child.style.color = "var(--text)";
-      },
-    );
+	_onSetSelected() {
+		// Update Label
+		const problemSetLabel = document.getElementById("problem-set-selected");
+		problemSetLabel.innerText = this.problemSetManager.selectedSet.title;
 
-    console.log(this.problemSetManager.selectedSet);
-  }
+		// Highlight Selected
+		Array.from(document.getElementById("problem-set-select").querySelector(".dropdown-content").children).forEach(
+			(child) => {
+				if (child.innerText === this.problemSetManager.selectedSet.title) child.style.color = "var(--rose)";
+				else child.style.color = "var(--text)";
+			},
+		);
 
-  setup() {
-    this.problemSetManager.addEventListener("setsLoaded", this._onSetsLoaded.bind(this));
-    this.problemSetManager.addEventListener("setSelected", this._onSetSelected.bind(this));
-  }
+		console.log(this.problemSetManager.selectedSet);
+	}
 
-  /**
-   * @param {KeyboardEvent} ev
-   */
-  keydown(ev) {}
+	setup() {
+		this.problemSetManager.addEventListener("setsLoaded", this._onSetsLoaded.bind(this));
+		this.problemSetManager.addEventListener("setSelected", this._onSetSelected.bind(this));
+	}
 
-  lostFocus() {}
+	/**
+	 * @param {KeyboardEvent} ev
+	 */
+	keydown(ev) {
+		let key = ev.key;
+		let newMap = this.currentKeyMap[key];
+
+		if (!newMap) {
+			this.currentKeyMap = this.keyMappings;
+			return;
+		}
+	}
+
+	lostFocus() { }
 }
