32e9751d44efe35081ea9afa941d8265e55da7fd
Harrison Cook <HonsonCooky@gmail.com>
2024-07-21 15:53:13 +1200

    rm prettier

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    "path-to-global-node-modules\\prettier-plugin-css-order\\src\\main.mjs",
-    "path-to-global-node-modules\\prettier-plugin-organize-attributes\\lib\\index.js",
-  ],
-  printWidth: 120,
-  proseWrap: "always",
-};
